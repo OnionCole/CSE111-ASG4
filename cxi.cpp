a14000b5a977e85400b6f8d184cd010731a3a899
Cole Anderson <cole@cairene.net>
2022-03-07 21:41:19 -0800

    CXI: Did cxi_rm, and fixed main to handle the fn

--- cxi.cpp
+++ cxi.cpp
@@ -47,6 +47,39 @@ void cxi_help() {
    cout << help;
 }
 
+
+void cxi_put(client_socket& server, char* fn) {
+   // fn is ready to go into the header
+
+
+}
+
+void cxi_get(client_socket& server, char* fn) {
+   // fn is ready to go into the header
+
+
+}
+
+void cxi_rm(client_socket& server, char* fn) {
+   // fn is ready to go into the header
+
+   cxi_header hdr;
+   hdr.filename = fn;
+   hdr.command = cxi_command::RM;
+   hdr.nbytes = 0;
+   send_packet(server, &hdr, sizeof hdr);
+   
+   recv_packet(server, &hdr, sizeof hdr);
+   if (hdr.command == cxi_command::NAK) {
+      cout << "RM: FAILURE: NAK: errno:" << 
+            strerror(ntohl(hdr.nbytes)) << endl;
+   } else if (hdr.command == cxi_command::ACK) {
+      cout << "RM: SUCCESS: ACK" << endl;
+   } else {
+      cout << "RM: UNCERTAIN: recieved neither NAK nor ACK" << endl;
+   }
+}
+
 void cxi_ls (client_socket& server) {
    cxi_header header;
    header.command = cxi_command::LS;
@@ -103,7 +136,25 @@ int main (int argc, char** argv) {
          getline (cin, line);
          if (cin.eof()) throw cxi_exit();
 
-         const auto& itor = command_map.find (line);
+         // get com and fn (DESTROYS line) (com means command)
+         string com = line;
+         string fn = "";
+         int spos = line.find(" ");
+         if (spos != string::npos) {  // found a whitespace
+            com = line.substr(0, spos);
+            line.erase(0, spos + 1);
+            fn = line;
+         }
+
+         // put fn into a char*
+         if (fn.length() > 58) {  // too long
+            cout << "Err: fn:" << fn << ", is >58 chars long" << endl;
+            continue;
+         }
+         char fn_cstr[FILENAME_SIZE] {};
+         strncpy(fn_cstr, fn.c_str(), FILENAME_SIZE);
+
+         const auto& itor = command_map.find (com);
          cxi_command cmd = itor == command_map.end()
                          ? cxi_command::ERROR : itor->second;
          switch (cmd) {
@@ -114,19 +165,19 @@ int main (int argc, char** argv) {
                cxi_help();
                break;
             case cxi_command::PUT:
-               cxi_put(server);
+               cxi_put(server, fn_cstr);
                break;
             case cxi_command::GET:
-               cxi_get(server);
+               cxi_get(server, fn_cstr);
                break;
             case cxi_command::RM:
-               cxi_rm(server);
+               cxi_rm(server, fn_cstr);
                break;
             case cxi_command::LS:
                cxi_ls (server);
                break;
             default:
-               outlog << line << ": invalid command" << endl;
+               outlog << com << ": invalid command" << endl;
                break;
          }
       }
